9b7841a4f2796ed7c0cc09d013a235f4805b9cd5
saleha <per.saleha@gmail.com>
2025-02-21 16:44:19 +0300

    [s] add cam calibration data and helping files

---
+++ ROS_com.bash
@@ -0,0 +1,4 @@
+# Check the RP IP first by: `ip a`  
+export ROS_DOMAIN_ID=1
+export ROS_IP=192.168.100.173
+export ROS_HOSTNAME=192.168.100.173
--- run.bash
+++ run.bash
@@ -2,12 +2,25 @@
 source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 
-#ros2 run opencv_cam opencv_cam_main >> worked and show output of camera
+#ros2 run usb_cam usb_cam_node_exe --ros-args --params-file /home/RPViam/saleha/SDP/src/usb_cam/config/myParam.yaml
+
+#ros2 run rqt_image_view rqt_image_view
+
+ros2 launch ros2_aruco aruco_recognition.launch.py
+
+#ros2 run ros2_aruco aruco_node --ros-args -p image_topic:=/image_raw 
+
+#ros2 run ros2_aruco aruco_generate_marker --dictionary  DICT_4X4_250
+
+# worked and show output of camera
+#ros2 run opencv_cam opencv_cam_main --ros-args --params-file /home/RPViam/saleha/SDP/ost.yaml
+#ros2 run opencv_cam opencv_cam_main
 #ros2 run usb_cam usb_cam_node --ros-args -p video_device:=/dev/video0
 
 
-ros2 run usb_cam usb_cam_node_exe
- #ros2 run opencv_cam opencv_cam_main
+#ros2 run usb_cam usb_cam_node_exe
+#ros2 run opencv_cam opencv_cam_main
 
 # This worked also and show the readinga 
 #ros2 run camera_calibration cameracalibrator  --no-service-check --size 10x14 --square 0.02 --charuco_marker_size 0.015 --aruco_dict 4x4_50  -p charuco --ros-args -r image:=/image_raw 
+#ros2 run camera_calibration cameracalibrator  --no-service-check --size 7x9 --square 0.02 -p chessboard --ros-args -r image:=/image_raw 

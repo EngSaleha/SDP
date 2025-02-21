95e5a261b277b99ecab30adcc4aafa64eec26ac5
saleha <per.saleha@gmail.com>
2025-02-21 16:36:57 +0300

    [S] add camera and Aruco packages

--- run.bash
+++ run.bash
@@ -6,7 +6,8 @@ source install/setup.bash
 #ros2 run usb_cam usb_cam_node --ros-args -p video_device:=/dev/video0
 
 
-# ros2 run opencv_cam opencv_cam_main
+ros2 run usb_cam usb_cam_node_exe
+ #ros2 run opencv_cam opencv_cam_main
 
 # This worked also and show the readinga 
-ros2 run camera_calibration cameracalibrator  --no-service-check --size 10x14 --square 0.02 --charuco_marker_size 0.015 --aruco_dict 4x4_50  -p charuco --ros-args -r image:=/image_raw 
+#ros2 run camera_calibration cameracalibrator  --no-service-check --size 10x14 --square 0.02 --charuco_marker_size 0.015 --aruco_dict 4x4_50  -p charuco --ros-args -r image:=/image_raw 
